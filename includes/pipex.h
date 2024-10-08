3a1124422783d5eff5cac1b3fde46262c92f7436
xenia-0101 <xeniavislocka@gmail.com>
2024-10-08 16:26:44 +0200

    structure

---
+++ includes/pipex.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xenia <xenia@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/07 11:23:14 by xenia             #+#    #+#             */
+/*   Updated: 2024/10/07 19:42:03 by xenia            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+/// includes
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <fcntl.h>
+
+#endif
