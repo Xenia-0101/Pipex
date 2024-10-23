faa0b32c08c715a4706c14daa4153bbdf11eb4e0
xenia-0101 <xeniavislocka@gmail.com>
2024-10-23 16:41:21 +0200

    Create basic logic

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: xenia <xenia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 16:29:41 by xenia             #+#    #+#             */
-/*   Updated: 2024/10/23 14:45:07 by xenia            ###   ########.fr       */
+/*   Updated: 2024/10/23 16:37:38 by xenia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,30 @@ void	ft_pipex(t_map *map,int argc, char *argv[])
 
 int	main(int argc, char *argv[], char *envp[])
 {
+	t_map	map;
+	int		i;
+
+	if (argc < 5)
+	{
+		ft_put_error("Incorrect number of arguments");
+		exit(1);
+	}
+	ft_init_map(&map);
+	// check and open files
+	if (ft_open_files(&map, argv[1], argv[argc - 1]))
+		exit(1);
+	// get paths
+	ft_get_paths(&map, envp);
+	// redirect infile to stdin
+	dup2(map.in_fd, STDIN_FILENO);
+	i = 2;
+	// execute commands
+	while (i < argc - 1)
+	{
+		ft_exec_cmd(&map, argv[i], envp);
+		i++;
+	}
+
 
 	return (0);
 }
---
+++ src/ft_exec_cmd.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_exec_cmd.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xenia <xenia@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/23 16:31:14 by xenia             #+#    #+#             */
+/*   Updated: 2024/10/23 16:35:06 by xenia            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/pipex.h"
+
+void	ft_exec_cmd(t_map *map, char *cmd, char *env[])
+{
+	// create pipe
+	// fork
+	// child -> process cmd
+	//		 -> loop paths and execute
+	// parent -> redirect to stdin
+}
