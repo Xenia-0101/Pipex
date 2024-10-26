49d0c6126a9cdc1d74e62a78c1598b5c6a9f2b0f
xenia-0101 <xeniavislocka@gmail.com>
2024-10-26 22:37:52 +0200

    fix leaks

--- src/ft_get_full_path.c
+++ src/ft_get_full_path.c
@@ -6,7 +6,7 @@
 /*   By: xenia <xenia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 09:15:56 by xenia             #+#    #+#             */
-/*   Updated: 2024/10/26 22:24:08 by xenia            ###   ########.fr       */
+/*   Updated: 2024/10/26 22:32:01 by xenia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	ft_get_full_path(t_map *map, char *cmd)
 	map->cmd_args = ft_split(cmd, ' ');
 	map->full_path = ft_strdup(cmd);
 	i = 0;
-	while (map->paths[i])
+	while (i < ft_arrlen(map->paths) + 1)
 	{
 		if (!access(map->full_path, F_OK) & !access(map->full_path, X_OK))
 		{
--- src/ft_utils.c
+++ src/ft_utils.c
@@ -6,7 +6,7 @@
 /*   By: xenia <xenia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 16:09:53 by xenia             #+#    #+#             */
-/*   Updated: 2024/10/25 23:59:15 by xenia            ###   ########.fr       */
+/*   Updated: 2024/10/26 22:31:09 by xenia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,3 +34,18 @@ void	ft_put_error_2(char *error, char *val)
 	write(2, val, len);
 	write(2, "\033[0m\n", 6);
 }
+
+int	ft_arrlen(char **arr)
+{
+	int	i;
+
+	i = 0;
+	if (arr != NULL)
+	{
+		while (arr[i])
+		{
+			i++;
+		}
+	}
+	return (i);
+}	
