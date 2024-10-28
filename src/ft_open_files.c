73ab282d6bcea359f13d79663b52f6d3afa1621c
xenia-0101 <xeniavislocka@gmail.com>
2024-10-28 08:48:33 +0100

    Refactor ft_open_files for the norm and modularity.

--- src/ft_open_files.c
+++ src/ft_open_files.c
@@ -6,11 +6,39 @@
 /*   By: xenia <xenia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 15:42:44 by xenia             #+#    #+#             */
-/*   Updated: 2024/10/27 23:03:06 by xenia            ###   ########.fr       */
+/*   Updated: 2024/10/28 08:40:44 by xenia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
+
+void	ft_check_fd(t_map *map, char *f1, char *f2)
+{
+	if (map->in_fd == -1)
+	{
+		perror(f1);
+		map->in_fd = open("/dev/null", O_RDONLY);
+	}
+	if (map->out_fd == -1)
+	{
+		perror(f2);
+		exit(1);
+	}
+}
+
+void	ft_read_here_doc(t_map *map)
+{
+	char *input;
+
+	input = get_next_line(STDIN_FILENO);
+	while (strncmp(input, map->argv[2], ft_strlen_gnl(map->argv[2])))
+	{
+		write(map->in_fd, input, ft_strlen_gnl(input));
+		free(input);
+		input = get_next_line(STDIN_FILENO);
+	}
+	free(input);
+}
 
 /**
  * @brief	Open infile and outfile.
@@ -22,43 +50,21 @@
  */
 int	ft_open_files(t_map *map, char *f1, char *f2)
 {
-	// check if "here_doc"
-	// -- compare bytes
-	// -- compare length
-	// check if limiter
-	// -- argc > 5
-	// save limiter
 	if (!ft_strncmp(f1, "here_doc", 8) && (ft_strlen(f1) == 8)
 		&& map->argc > 5)
 	{
-		char *input;
-		// open here_doc
 		map->in_fd = open("here_doc", O_WRONLY | O_CREAT | O_TRUNC, 0644);
-		// read from std in
-		input = get_next_line(STDIN_FILENO);
-		// -- check for limiter
-		while (strncmp(input, map->argv[2], ft_strlen_gnl(map->argv[2])))
-		{
-			write(map->in_fd, input, ft_strlen_gnl(input));
-			free(input);
-			input = get_next_line(STDIN_FILENO);
-		}
-		// write to here_doc
-		map->in_fd = open("here_doc", O_RDONLY, 0644);
-		map->out_fd = open(f2, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		ft_read_here_doc(map);
+		map->in_fd = open("here_doc", O_RDONLY, 0666);
+		map->out_fd = open(f2, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+		ft_check_fd(map, f1, f2);
 		return (3);
 	}
-	map->in_fd = open(f1, O_RDONLY, 0644);
-	map->out_fd = open(f2, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (map->in_fd == -1)
+	else
 	{
-		perror(f1);
-		map->in_fd = open("/dev/null", O_RDONLY);
-	}
-	if (map->out_fd == -1)
-	{
-		perror(f2);
-		exit(1);
+		map->in_fd = open(f1, O_RDONLY, 0666);
+		map->out_fd = open(f2, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+		ft_check_fd(map, f1, f2);
 	}
 	return (2);
 }
